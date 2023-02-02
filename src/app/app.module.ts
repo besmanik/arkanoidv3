1b7d7d05ab5a63e7949321e2fd13e0b1346c297d
Daniyar Turak <dany.turak@gmail.com>
2023-02-03 01:38:37 +0600

    feat: ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BricksComponent } from './components/bricks/bricks.component';
+import { BricksEffects } from './store/bricks/bricks.effects';
+import { bricksReducer } from './store/bricks/bricks.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BricksComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ bricks: bricksReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+    }),
+    EffectsModule.forRoot([BricksEffects]),
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/bricks/bricks.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { loadBricks, destroyBrick } from 'src/app/store/bricks/bricks.actions';
+import { selectAllBricks } from 'src/app/store/bricks/bricks.selectors';
+
+@Component({
+  selector: 'mc-bricks',
+  templateUrl: './bricks.component.html',
+  styleUrls: ['./bricks.component.scss'],
+})
+export class BricksComponent implements OnInit {
+  public allBricks$ = this.store.select(selectAllBricks);
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(loadBricks());
+    console.log(this.allBricks$);
+  }
+
+  destroyBrick(): void {
+    this.store.dispatch(destroyBrick({ id: 2 }));
+  }
+}
